7ffdc0f595ff22c78b12e9a67461cfee3fa136d1
safira.pusparanti <safira.pusparanti@gmail.com>
2017-09-16 01:39:42 +0700

    First commit

---
+++ src/main/java/com/example/demo/SpringTutorial2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorial2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutorial2Application.class, args);
+	}
+}
